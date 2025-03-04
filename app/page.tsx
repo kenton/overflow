74ded1a2ef54ed2c698c1c89497ef8d4fead8152
Kenton Newby <kentonnewby@gmail.com>
2025-03-04 17:11:25 -0500

    Implements light-dark mode selector

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Home = () => {
     return (
         <>
-            <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+            <h1 className="h1-bold"></h1>
         </>
     )
 }
