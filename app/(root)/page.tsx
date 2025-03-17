ca907ee8566d237635767272fb588eeede81a957
Kenton Newby <kentonnewby@gmail.com>
2025-03-17 12:31:43 -0400

    Implements auth using github and google

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
 import Image from "next/image";
+import {auth, signOut} from "@/auth";
+import {Button} from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
-const Home = () => {
-    return (
-        <>
-            <h1 className="h1-bold"></h1>
-        </>
-    )
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+      <>
+        <h1 className="h1-bold">Welcome to Overflow</h1>
+        <form className="px-10 pt-[100px]" action={async () => {
+          'use server';
+
+          await signOut({redirectTo: ROUTES.SIGN_IN});
+        }}>
+          <Button type="submit">Logout</Button>
+        </form>
+      </>
+  )
 }
 
 export default Home;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,41 +2,50 @@ import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
+import {Toaster} from "@/components/ui/sonner";
+import {SessionProvider} from "next-auth/react";
+import {auth} from "@/auth";
 
 const inter = localFont({
-    src: "./fonts/InterVF.ttf",
-    variable: "--font-inter",
-    weight: "100 200 300 400 500 600 700 800 900",
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 
 const spaceGrotesk = localFont({
-    src: "./fonts/SpaceGroteskVF.ttf",
-    variable: "--font-space-grotesk",
-    weight: "300 400 500 700",
+  src: "./fonts/SpaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 700",
 });
 
 export const metadata: Metadata = {
-    title: "Overflow",
-    description: "A no-nonsense platform where developers get straight answers to their programming questions. Connect with a global community, share knowledge, and collaborate on everything from web and mobile development to algorithms and data structures—no fluff, just solutions.",
-    icons: {
-        icon: "/images/site-logo.svg",
-    },
+  title: "Overflow",
+  description: "A no-nonsense platform where developers get straight answers to their programming questions. Connect with a global community, share knowledge, and collaborate on everything from web and mobile development to algorithms and data structures—no fluff, just solutions.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
-export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <html lang="en" suppressHydrationWarning>
+// const RootLayout = () => {}
+// export default async function RootLayout
+
+const RootLayout = async ({children}: { children: React.ReactNode }
+) => {
+  const session = await auth();
+  return (
+      <html lang="en" suppressHydrationWarning>
+      <SessionProvider session={session}>
         <body
             className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
         >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-            {children}
+          {children}
         </ThemeProvider>
+        <Toaster/>
         </body>
-        </html>
-    );
+      </SessionProvider>
+      </html>
+  );
 }
+
+export default RootLayout;
